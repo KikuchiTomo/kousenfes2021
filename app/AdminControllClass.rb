dc8d8a17091727b617a7b64c40bf48d59b2f0326
kouhou <kouhou@ik1-301-10947.vs.sakura.ne.jp>
2021-10-21 01:49:29 +0900

    お知らせ管理機能を追加(新規作成・編集・削除・一覧)

--- app/AdminControllClass.rb
+++ app/AdminControllClass.rb
@@ -65,10 +65,10 @@ class AdminControllClass < QRControllClass
           return false
         end
     
-        puts "You logined : OK #{uuid_admin}"
+        puts "You logined : OK #{uuid_admin} : #{Time.now}"
         return true
     end
-    
+
     get '/kousen/admin/0AEECC00-4D5E-5043-E97C-2CBF5A8B2356/STN/login' do
         erb :admin_login
     end
@@ -98,18 +98,26 @@ class AdminControllClass < QRControllClass
         session[:token_admin] = ses[:token_admin] 
         session[:expire_admin]= ses[:expire_admin]
 
-        puts "#{ses[:token_admin]}"
-        #isLogined = checkAdminToken('/kousen/admin/0AEECC00-4D5E-5043-E97C-2CBF5A8B2356/STN/login')
-        #return "ERROR : session is broken" if !isLogined
+        # nilなら'0'にして権限剥奪
+        session[:permit_user]  = row['permit_user'] ||=0
+        session[:permit_bingo] = row['permit_bingo']||=0
+        session[:permit_proj]  = row['permit_proj'] ||=0
+        session[:permit_news]  = row['permit_news'] ||=0
+        session[:permit_qr]    = row['permit_qr']   ||=0
+        session[:permit_admin] = row['permit_admin']||=0
 
+        session[:first_name]   = row['first_name']  ||='STRING_ERROR'
+        session[:last_name]    = row['last_name']   ||='STRING_ERROR'
+        puts "#{ses[:token_admin]}"
+        
         redirect to('/kousenadmin/dashboard')
     end
 
     get '/kousenadmin/logout' do
         uuid_admin = session[:uuid_admin]
-        session[:uuid_admin] = nil
-        session[:email_admin] = nil
-        session[:token_admin] = nil
+        session[:uuid_admin]   = nil
+        session[:email_admin]  = nil
+        session[:token_admin]  = nil
         session[:expire_admin] = nil
 
         if uuid_admin!=nil&&uuid_admin!=''
@@ -118,20 +126,182 @@ class AdminControllClass < QRControllClass
         else
             puts "UUID IS NIL!!!"
         end
+
         redirect '/kousen/admin/0AEECC00-4D5E-5043-E97C-2CBF5A8B2356/STN/login'
     end 
 
-
-
     get '/kousenadmin/dashboard' do
+        uuid_admin = session[:uuid_admin]
         puts "HELLO THIS IS DASHBOARD! "
         isLogined = checkAdminToken('/kousenadmin/logout')
         return "ERROR : session is broken" if !isLogined
         puts "TOKEN ADMIN => #{isLogined}"
+        
+        @name = session[:first_name]
+        @permit = {
+          :user  => session[:permit_user],
+          :bingo => session[:permit_bingo],
+          :news  => session[:permit_news],
+          :qr    => session[:permit_qr],
+          :proj  => session[:permit_proj],
+          :admin => session[:permit_admin]
+        }
+        
+        sql = 'select id from kouhou.users where del_flg=0 and status=2;'
+        @acc_cnt = execSql(sql).count ||=0
         erb :admin_dashboard
     end
 
     after do
         cache_control :no_cache
     end
+
+    #========================START : NEWS=====================================
+    get '/kousenadmin/news/dashboard' do
+      checkAdminToken('/kousenadmin/logout')
+      permit = session[:permit_news]
+      return "Permission denied : YOU CANT CONTROL NEWS" if permit==0
+
+      sql = 'select * from kouhou.news where del_flg=0 order by notify_datetime desc;'
+      res = execSql(sql)
+      @news_list = []
+      if res.count<1
+        @news_list = nil
+      else
+        res.each{ |news|
+          hash = {}
+          hash['id'] = news['id']
+          hash['title'] = news['title']
+          hash['dtitle'] = news['detail_title']
+          hash['date'] = news['notify_datetime']
+          hash['desc'] = news['description']
+          hash['ddesc'] = news['detail_desc']
+          hash['ena'] = news['ena_flg']
+          @news_list.push(hash)
+        }
+      end
+
+      erb :admin_news_list
+    end
+
+    post '/kousenadmin/news/update/showing_list' do
+      checkAdminToken('/kousenadmin/logout')
+      permit = session[:permit_news]
+      return "Permission denied : YOU CANT CONTROL NEWS" if permit==0
+
+      ena_ids = params['enable'] ||=''
+      
+      return "PARAMS DONT HAVE ANT DATA" if ena_ids==''
+
+      sql = 'select id from kouhou.news where del_flg=0;'
+      res = execSql(sql)
+      
+      return "DB DONT HAS ANY DATA" if(res.count<1)
+      # return "PARAMS WAS BROKEN"    if(res.count!=ena_ids.count)
+
+      sql = 'update kouhou.news set ena_flg=? where del_flg=0 and id=?;'    
+      
+      res.each{ |row|
+        
+        if ena_ids.include?(row['id'].to_s)
+          execSql(sql, 1, row['id'])
+        else
+          execSql(sql, 0, row['id'])
+        end
+      }
+
+      redirect to('/kousenadmin/news/dashboard')
+    end
+
+    get '/kousenadmin/news/update/edit' do
+      checkAdminToken('/kousenadmin/logout')
+      permit = session[:permit_news]
+      return "Permission denied : YOU CANT CONTROL NEWS" if permit==0
+
+      id = params['id'] ||=''
+      return "NEED ARGS : NEWS ID" if id==''
+
+      sql = 'select * from kouhou.news where del_flg=0 and id=?;'
+      res = execSql(sql, id)
+      return "DB IS BROKEN" if res.count!=1
+
+      row = res.first
+      @is_create = false
+      @news = {}
+      @news['id'] = row['id']
+      @news['title'] = row['title']
+      @news['dtitle'] = row['detail_title']
+      @news['desc'] = row['description']
+      @news['ddesc'] = row['detail_desc']
+      @news['date'] = Time.parse(row['notify_datetime'].to_s)
+      @news['path'] = row['image_path']
+
+      erb :admin_news_edit
+    end
+
+    get '/kousenadmin/news/update/add' do
+      checkAdminToken('/kousenadmin/logout')
+      permit = session[:permit_news]
+      return "Permission denied : YOU CANT CONTROL NEWS" if permit==0
+      @is_create = true
+      erb :admin_news_add
+    end
+
+    post '/kousenadmin/news/update/edit' do
+      checkAdminToken('/kousenadmin/logout')
+      permit = session[:permit_news]
+      return "Permission denied : YOU CANT CONTROL NEWS" if permit==0
+
+      id    = params['id'] ||=''
+      title = params['title'] ||=''
+      dtitle= params['detail_title']||=''
+      desc  = params['description'] ||=''
+      ddesc = params['detail_desc'] ||=''
+      date  = params['notify_datetime'] ||=''
+
+      return "NEED ARGS : ANY" if(title==''||dtitle==''||desc==''||ddesc==''||date==''||id=='')
+      return "TOO LONG DATA : ANY" if(title.size>60)
+
+      notify_time = Time.parse(date)
+      
+      sql = 'update kouhou.news set title=?,detail_title=?,description=?,detail_desc=?,notify_datetime=? where id=?;'
+      execSql(sql, title, dtitle, desc, ddesc, date, id);
+      redirect to('/kousenadmin/news/dashboard')
+    end
+
+    get '/kousenadmin/news/update/delete' do
+      checkAdminToken('/kousenadmin/logout')
+      permit = session[:permit_news]
+      return "Permission denied : YOU CANT CONTROL NEWS" if permit==0
+
+      id = params['id'] ||=''
+      return "NEED ARG" if id==''
+
+      sql = 'update kouhou.news set del_flg=1 where id=?;'
+      execSql(sql, id)
+      redirect to('/kousenadmin/news/dashboard')
+    end
+
+    post '/kousenadmin/news/update/add' do
+      checkAdminToken('/kousenadmin/logout')
+      permit = session[:permit_news]
+      return "Permission denied : YOU CANT CONTROL NEWS" if permit==0
+
+      img   = params['image_path'] ||='NOIMAGE'
+      title = params['title'] ||=''
+      dtitle= params['detail_title']||=''
+      desc  = params['description'] ||=''
+      ddesc = params['detail_desc'] ||=''
+      date  = params['notify_datetime'] ||=''
+
+      return "NEED ARGS : ANY" if(title==''||dtitle==''||desc==''||ddesc==''||date=='')
+      return "TOO LONG DATA : ANY" if(title.size>60)
+
+      notify_time = Time.parse(date)
+      
+      sql = 'insert into kouhou.news (title,detail_title,description,detail_desc,notify_datetime,image_path) values (?,?,?,?,?,?);'
+      execSql(sql, title, dtitle, desc, ddesc, date, img);
+      redirect to('/kousenadmin/news/dashboard')
+    end
+    #=============================END : NEWS=====================================
 end
